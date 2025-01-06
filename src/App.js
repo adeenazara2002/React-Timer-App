c0dc81ee73b34fb6eb2f8b04362f4b3e659d46cd
adeenazara2002 <adinazara2001@gmail.com>
2025-01-06 20:36:33 +0500

    Timer App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Background from "./Components/Background";
+
+function App() {
+  return (
+    <div>
+      <Background />
+      {/* Add your timer UI or other content here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Background.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Background.css";
+
+const Background = () => {
+  return (
+    <div className="background">
+      {/* Add any other UI elements here */}
+    </div>
+  );
+};
+
+export default Background;
